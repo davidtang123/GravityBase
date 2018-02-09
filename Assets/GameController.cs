7bdfbcd3a8a1d7d7be35f3fd9aeceb1fc51db21f
DTang <davidtang997@gmail.com>
2018-02-08 19:48:49 -0500

    Part 2 Completed: Draws ray (based on sprite that can be changed) Ray points "up"

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -2,18 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(AudioSource))]
+//[RequireComponent(typeof(AudioSource))]
 public class GameController : MonoBehaviour {
     AudioSource ding;
 	// Use this for initialization
 	void Start () {
-        //WWW audioLoader = new WWW("file:///C:/Users/David/Documents/GravityBase/Assets/Sounds");
 
-        ding = GetComponent<AudioSource>();
     }
 	
 	// Update is called once per frame
 	void Update () {
+        
         for (int i = 0; i < Input.touchCount; ++i)
         {
             if (Input.GetTouch(i).phase == TouchPhase.Began)
@@ -22,9 +21,27 @@ public class GameController : MonoBehaviour {
                 Handheld.Vibrate();
                 // Construct a ray from the current touch coordinates
                 //Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(i).position);
-                // Create a particle if hit
-                //if (Physics.Raycast(ray))
-                //    Instantiate(particle, transform.position, transform.rotation);
+                //Vector3 touchpos = new Vector3(Input.GetTouch(i).position.x, Input.GetTouch(i).position.y, 0);
+
+                //Gizmos.color = Color.red;
+                //Gizmos.DrawRay(transform.position, (touchpos - transform.position)*10);
+                //Debug.Log(Input.GetTouch(i).position.x + ", " + Input.GetTouch(i).position.y);
+
+
+                /*
+                GameObject ray = new GameObject();
+                ray.transform.position = this.transform.position;
+                ray.AddComponent<LineRenderer>();
+                LineRenderer lr = ray.GetComponent<LineRenderer>();
+                lr.material = new Material(Shader.Find("Particles/Alpha Blended Premultiply"));
+                lr.startColor = Color.red;
+                lr.endColor = Color.red;
+                lr.startWidth = 0.5f;
+                lr.endWidth = 0.5f;
+                lr.SetPosition(0, transform.position);
+                lr.SetPosition(1, new Vector3(5,5,0));
+                //GameObject.Destroy(ray, 2);
+                */
             }
         }
     }
